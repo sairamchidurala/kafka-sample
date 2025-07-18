4ab4c3e2afd41576d019a2e3df3db2b61ed67aab
Sairam Chidurala <sairam.chidurala@nextiva.com>
2025-07-19 01:15:19 +0530

    Created working docker

--- service/src/test/java/com/learn/service/ServiceApplicationTests.java
+++ service/src/test/java/com/learn/service/ServiceApplicationTests.java
@@ -6,17 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ServiceApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() throws Exception {
 		String data = """
 				{"source":"whatsapp","payload":{"object":"whatsapp_business_account","entry":[{"id":"992057311587620","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"12143969525","phone_number_id":"547643665095640"},"contacts":[{"profile":{"name":"hellokiller63"},"wa_id":"919985235601"}],"messages":[{"from":"919985235601","id":"wamid.HBgMOTE5OTg1MjM1NjAxFQIAEhgUM0EwQ0U4NjhEOUZBQjNBODRDMzIA","timestamp":"1741242016","text":{"body":"abcdefg"},"type":"text"}]},"field":"messages"}]}]}}
 				""";
-//		String data = """
-//				{"source":"messenger","payload":{"object":"page","entry":[{"time":1740984381602,"id":"419537554568805","messaging":[{"sender":{"id":"8300520303390501"},"recipient":{"id":"419537554568805"},"timestamp":1740984380942,"message":{"mid":"m_rv2AVR_5hthZrGawaa80kKXpvyoW4ey9lzg3I7ClM24XbY_hDkXgGQqtc33claVf03ABCYfQAL8FVDN4vyY9jg","attachments":[{"type":"image","payload":{"url":"https://scontent.xx.fbcdn.net/v/t1.15752-9/480937815_949200667372913_3748486160290402352_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=fc17b8&_nc_ohc=ek78920ojNEQ7kNvgEKjZMR&_nc_oc=AdhoIDu0h4kI9L1Y78f3U8-dx_NoVtu7J9r2CLhHAW-S_MIDe81mdlL7OIvJaj3FMcOl6Y9McOlySBvozLaQR7Tw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFWdS6XSN_tA6QJdryqVSABHNSCF6zDXlTca914bvXv_w&oe=67ECC107"}}]}}]}]}}
-//				""";
+		// String data = """
+		// {"source":"messenger","payload":{"object":"page","entry":[{"time":1740984381602,"id":"419537554568805","messaging":[{"sender":{"id":"8300520303390501"},"recipient":{"id":"419537554568805"},"timestamp":1740984380942,"message":{"mid":"m_rv2AVR_5hthZrGawaa80kKXpvyoW4ey9lzg3I7ClM24XbY_hDkXgGQqtc33claVf03ABCYfQAL8FVDN4vyY9jg","attachments":[{"type":"image","payload":{"url":"https://scontent.xx.fbcdn.net/v/t1.15752-9/480937815_949200667372913_3748486160290402352_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=fc17b8&_nc_ohc=ek78920ojNEQ7kNvgEKjZMR&_nc_oc=AdhoIDu0h4kI9L1Y78f3U8-dx_NoVtu7J9r2CLhHAW-S_MIDe81mdlL7OIvJaj3FMcOl6Y9McOlySBvozLaQR7Tw&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFWdS6XSN_tA6QJdryqVSABHNSCF6zDXlTca914bvXv_w&oe=67ECC107"}}]}}]}]}}
+		// """;
 
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode rootNode = objectMapper.readTree(data);
--- webhook/src/main/java/com/learn/myproj/Configurations/KafkaConfig.java
+++ webhook/src/main/java/com/learn/myproj/Configurations/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // Use String Serializer
 
